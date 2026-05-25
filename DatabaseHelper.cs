9938e8f9ca047a2f89eba40147de09237c785a2b
LuciArd3 <prxd.cxld@gmail.com>
2026-05-25 11:03:11 +0300

    pt3- complet

--- DatabaseHelper.cs
+++ DatabaseHelper.cs
@@ -1,68 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Data.Sqlite;
+using System.Data;
+using System.Data.OleDb;
 
 namespace PROIECT_PAW
 {
     public class DatabaseHelper
     {
-        private const string FISIER_DB = "hotel.db";
+        private const string FISIER_DB = "hotel.accdb";
         private string ConnectionString =>
-            $"Data Source={FISIER_DB};";
+            $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={FISIER_DB};";
+
         public void CreeazaTabel()
         {
-            using (var con = new SqliteConnection(ConnectionString))
+            using (var con = new OleDbConnection(ConnectionString))
             {
                 con.Open();
                 string sql = @"
-                    CREATE TABLE IF NOT EXISTS Rezervari (
+                    CREATE TABLE Rezervari (
                         Id          INTEGER PRIMARY KEY,
-                        NumeClient  TEXT    NOT NULL,
-                        Sex         TEXT    NOT NULL,
-                        Telefon     TEXT    NOT NULL,
+                        NumeClient  TEXT NOT NULL,
+                        Sex         TEXT NOT NULL,
+                        Telefon     TEXT NOT NULL,
                         NumarCamera INTEGER NOT NULL,
-                        TipCamera   TEXT    NOT NULL,
-                        PretNoapte  REAL    NOT NULL,
+                        TipCamera   TEXT NOT NULL,
+                        PretNoapte  DOUBLE NOT NULL,
                         Etaj        INTEGER NOT NULL,
-                        CheckIn     TEXT    NOT NULL,
-                        CheckOut    TEXT    NOT NULL,
-                        Status      TEXT    NOT NULL
-                    );";
-                new SqliteCommand(sql, con).ExecuteNonQuery();
+                        CheckIn     TEXT NOT NULL,
+                        CheckOut    TEXT NOT NULL,
+                        Status      TEXT NOT NULL
+                    )";
+                try
+                {
+                    new OleDbCommand(sql, con).ExecuteNonQuery();
+                }
+                catch { /* tabela există deja */ }
             }
         }
 
         public void Adauga(Rezervare r)
         {
-            using (var con = new SqliteConnection(ConnectionString))
+            using (var con = new OleDbConnection(ConnectionString))
             {
                 con.Open();
-                string sql = @"
-                    INSERT OR REPLACE INTO Rezervari
-                        (Id, NumeClient, Sex, Telefon,
-                         NumarCamera, TipCamera, PretNoapte, Etaj,
-                         CheckIn, CheckOut, Status)
-                    VALUES
-                        (@id, @nume, @sex, @tel,
-                         @numar, @tip, @pret, @etaj,
-                         @ci, @co, @status);";
+                string sql = @"INSERT INTO Rezervari
+                    (Id, NumeClient, Sex, Telefon,
+                     NumarCamera, TipCamera, PretNoapte, Etaj,
+                     CheckIn, CheckOut, Status)
+                    VALUES (?,?,?,?,?,?,?,?,?,?,?)";
 
-                using (var cmd = new SqliteCommand(sql, con))
+                using (var cmd = new OleDbCommand(sql, con))
                 {
-                    cmd.Parameters.AddWithValue("@id", r.Id);
-                    cmd.Parameters.AddWithValue("@nume", r.Client.Nume);
-                    cmd.Parameters.AddWithValue("@sex", r.Client.Sex.ToString());
-                    cmd.Parameters.AddWithValue("@tel", r.Client.NrTelefon);
-                    cmd.Parameters.AddWithValue("@numar", r.Camera.Numar);
-                    cmd.Parameters.AddWithValue("@tip", r.Camera.Tip);
-                    cmd.Parameters.AddWithValue("@pret", r.Camera.PretNoapte);
-                    cmd.Parameters.AddWithValue("@etaj", r.Camera.Etaj);
-                    cmd.Parameters.AddWithValue("@ci", r.DataCheckIn.ToString("yyyy-MM-dd"));
-                    cmd.Parameters.AddWithValue("@co", r.DataCheckOut.ToString("yyyy-MM-dd"));
-                    cmd.Parameters.AddWithValue("@status", r.Status);
+                    cmd.Parameters.AddWithValue("Id", r.Id);
+                    cmd.Parameters.AddWithValue("NumeClient", r.Client.Nume);
+                    cmd.Parameters.AddWithValue("Sex", r.Client.Sex.ToString());
+                    cmd.Parameters.AddWithValue("Telefon", r.Client.NrTelefon);
+                    cmd.Parameters.AddWithValue("NumarCamera", r.Camera.Numar);
+                    cmd.Parameters.AddWithValue("TipCamera", r.Camera.Tip);
+                    cmd.Parameters.AddWithValue("PretNoapte", r.Camera.PretNoapte);
+                    cmd.Parameters.AddWithValue("Etaj", r.Camera.Etaj);
+                    cmd.Parameters.AddWithValue("CheckIn", r.DataCheckIn.ToString("yyyy-MM-dd"));
+                    cmd.Parameters.AddWithValue("CheckOut", r.DataCheckOut.ToString("yyyy-MM-dd"));
+                    cmd.Parameters.AddWithValue("Status", r.Status);
                     cmd.ExecuteNonQuery();
                 }
             }
@@ -71,12 +70,11 @@ namespace PROIECT_PAW
         public List<Rezervare> GetToate()
         {
             var lista = new List<Rezervare>();
-
-            using (var con = new SqliteConnection(ConnectionString))
+            using (var con = new OleDbConnection(ConnectionString))
             {
                 con.Open();
-                string sql = "SELECT * FROM Rezervari ORDER BY Id;";
-                using (var reader = new SqliteCommand(sql, con).ExecuteReader())
+                string sql = "SELECT * FROM Rezervari ORDER BY Id";
+                using (var reader = new OleDbCommand(sql, con).ExecuteReader())
                 {
                     while (reader.Read())
                     {
@@ -91,44 +89,40 @@ namespace PROIECT_PAW
                             Convert.ToSingle(reader["PretNoapte"]),
                             Convert.ToInt32(reader["Etaj"])
                         );
-                        var rez = new Rezervare(
+                        lista.Add(new Rezervare(
                             Convert.ToInt32(reader["Id"]),
                             client, camera,
                             DateTime.Parse(reader["CheckIn"].ToString()),
                             DateTime.Parse(reader["CheckOut"].ToString()),
                             reader["Status"].ToString()
-                        );
-                        lista.Add(rez);
+                        ));
                     }
                 }
             }
             return lista;
         }
 
-
         public void Sterge(int id)
         {
-            using (var con = new SqliteConnection(ConnectionString))
+            using (var con = new OleDbConnection(ConnectionString))
             {
                 con.Open();
-                string sql = "DELETE FROM Rezervari WHERE Id = @id;";
-                using (var cmd = new SqliteCommand(sql, con))
+                using (var cmd = new OleDbCommand(
+                    "DELETE FROM Rezervari WHERE Id = ?", con))
                 {
-                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Parameters.AddWithValue("Id", id);
                     cmd.ExecuteNonQuery();
                 }
             }
         }
 
-
         public void StergeToate()
         {
-            using (var con = new SqliteConnection(ConnectionString))
+            using (var con = new OleDbConnection(ConnectionString))
             {
                 con.Open();
-                new SqliteCommand("DELETE FROM Rezervari;", con).ExecuteNonQuery();
+                new OleDbCommand("DELETE FROM Rezervari", con).ExecuteNonQuery();
             }
         }
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace PROIECT_PAW
         [STAThread]
         static void Main(string[] args)
         {
+    
             Application.EnableVisualStyles(); 
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
